f4de6adf00a62121af817f784bf43cf9262f23d3
hoangtuyet98 <hoangtuyet010498@gmail.com>
2023-05-13 17:15:06 +0700

    commit

---
+++ src/BT4/VongLapFor_Mang.java
@@ -0,0 +1,18 @@
+package BT4;
+
+public class VongLapFor_Mang {
+    public static void main(String[] args) {
+
+    for( int i=0;i<=50;i++){
+        int arr[]={i};
+        for(int j:arr){
+            System.out.println(j);
+        }
+        i+=1;
+    }
+
+
+
+    }
+
+}
